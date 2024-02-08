7ca6aba87d76cea4e263543f5f99e4ca031e10e4
Ankit77760 <ankitm7972@gmail.com>
2024-02-08 15:19:54 +0530

    Recursive Approach

--- Binary_Search.cpp
+++ Binary_Search.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//tc-O(log_2_N)
 int BinarySearch(int arr[],int target,int n){
     int low=0;
     int high=n-1;
@@ -20,6 +21,26 @@ int BinarySearch(int arr[],int target,int n){
         mid=(low+high)/2;
     }
     return -1;
+}
+
+
+//Recursive Approach
+int BinarySearchh(int arr,int low,int high,int target){
+    if(low>high){
+        return -1;
+    }
+    int mid=(low+high)/2;
+
+    if(arr[mid]==target){
+        return mid;
+    }
+    else if(target>arr[mid]){
+        BinarySearch(arr,mid+1,high,target);
+    }
+    else{
+        BinarySearch(arr,low,mid-1,target);
+    }
+
 }
 
 int main(){
